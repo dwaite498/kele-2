0dbc7d62823feff097c3c7879884ad394ebef6c4
David Waite <dwaite498@gmail.com>
2017-03-24 22:21:11 +0000

    fixed the Kele class and successfully returned the auth_token

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,18 @@
+require 'httparty'
 class Kele
-    include 'httparty'
+    include HTTParty
 
-    
    def initialize(email, password)
       @user = {'email': email, 'password': password}
-      response = self.class.post('https://www.bloc.io/api/v1/sessions', body: @user)
-      raise "invalid email or password" if response != 200
+      response = self.class.post(base_api('sessions'), body: @user)
+      raise "An error has occured" if response == 401
       @auth_token = response['auth_token']
       puts @auth_token
    end
+end
+
+private
+
+def base_api(target)
+   "https://www.bloc.io/api/v1/#{target}" 
 end
