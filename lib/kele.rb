d11765e302bacc996d37a4c8c6643c1f259e6611
David Waite <dwaite498@gmail.com>
2017-03-25 22:51:08 +0000

    WIP user retrieval

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,14 +1,27 @@
 require 'httparty'
+require 'json'
 class Kele
     include HTTParty
+    include JSON
 
    def initialize(email, password)
       @user = {'email': email, 'password': password}
       response = self.class.post(base_api('sessions'), body: @user)
       raise "An error has occured" if response == 401
       @auth_token = response['auth_token']
-      puts @auth_token
    end
+   
+   def get_me
+      headers = {
+         :content_type => 'application/json',
+         :authorization => @auth_token
+      }
+      response = self.class.get base_api("users/me"), headers
+      @user_email = JSON.parse(response.body)
+      puts @user_email
+      puts JSON.parse(response.body)
+   end
+   
 end
 
 private
