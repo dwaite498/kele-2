10d95daa841cf2961881c7c8a17c4f022699e406
David Waite <dwaite498@gmail.com>
2017-03-29 00:39:23 +0000

    finish roadmap methods

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -8,7 +8,7 @@ module Roadmap
    
    def get_checkpoint(checkpoint_id)
       response = self.class.get base_api("checkpoints/#{checkpoint_id}"), headers: { :authorization => @auth_token }
-      # raise "An error has occured" unless response.code.between?(200, 299)
+      raise "An error has occured" unless response.code.between?(200, 299)
       @checkpoint = JSON.parse(response.body)
    end
 end
