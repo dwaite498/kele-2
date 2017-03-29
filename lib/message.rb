61666e114e30d00bacee2eb8a6b9d14f33ad0469
David Waite <dwaite498@gmail.com>
2017-03-29 10:56:55 +0000

    fixed bug, will finish after other checkpoint submit

--- lib/message.rb
+++ lib/message.rb
@@ -5,7 +5,7 @@ module Message
        else
            request_url = "/message_threads"
        end
-       response = self.class.get( request_url, headers: { :authorization => @auth_token })
+       response = self.class.get( base_api(request_url), headers: { :authorization => @auth_token })
        @messages = JSON.parse(response.body)
    end
    
@@ -16,6 +16,14 @@ module Message
            "subject": subject,
            "message": message
        }
+       
+    #   values = '{
+#   "sender": "strong.bad@strongbadia.gov",
+#   "recipient_id": 2,
+#   "token": "abcd",
+#   "subject": "Royale with cheese",
+#   "stripped-text": "That's what they call a quarter pounder in France."
+# }'
        response = self.class.post(base_api("messages"), values, headers: { :authorization => @auth_token })
        puts response
    end
